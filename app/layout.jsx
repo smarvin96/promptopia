966b49e7826753f9e4f91bab232cbab2ef554141
Marvin Sprenger <sprenger-marvin@outlook.de>
2023-05-17 08:50:49 +0200

    project setup, bcrypt, mongodb, mongoose, next-auth, +folder/import  fixes

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Entdecke & Teile AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="de">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
